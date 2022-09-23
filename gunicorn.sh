eba96bddd0994bbdae9f6d169cb5342aa72bacfb
NigerianCoder <gbemilanre@gmail.com>
2022-09-23 02:21:05 +0100

    Gunicorn setup

---
+++ gunicorn.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source env/bin/activate
+
+source /usr/share/nginx/html/test_ci_cd/
+
+python3 manage.py makemigrations
+python3 manage.py migrate 
+
+echo "Migrations done"
+
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
+
+sudo service daemon-reload
+sudo service gunicorn start
+sudo service gunicorn enable
+
+echo "Gunicorn has been started"
+
+sudo service gunicorn status
+sudo service gunicorn restart 
